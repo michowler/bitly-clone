74a80910001aeeb7a4a30f6edbdb14b3e2ab765c
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-04-14 17:40:06 +0800

    remove secure random

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,4 @@
 require 'CSV'
-#require 'SecureRandom'
-
-
-# values = []
-# CSV.foreach("db/urls.csv") do |row|
-#    values << row[0].remove("(",")")
-# end
-
-# Url.transaction do 
-# 	values.each do |x|
-#   		Url.connection.execute ("INSERT INTO urls (long_url, short_url, click_count) 
-#   		VALUES ('#{x}', '#{SecureRandom.urlsafe_base64(4, false)}', '0')")
-#   	end
-# end
-
-require 'CSV'
 
 def shorten
 	return ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(7).join
